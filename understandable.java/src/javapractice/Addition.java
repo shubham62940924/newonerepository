33c8feacfe1d27579cd172c4359aa8e8e83f356e
first_name <ss329222@gmail.com>
2022-02-03 20:10:56 +0530

    added:addition.java

---
+++ understandable.java/src/javapractice/Addition.java
@@ -0,0 +1,13 @@
+package javapractice;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=5, b=7,c;
+		c= a+b;
+		System.out.println("sum of two no is:"+c);
+		// TODO Auto-generated method stub
+
+	}
+
+}

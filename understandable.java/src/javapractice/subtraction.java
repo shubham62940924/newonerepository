62839fbabf9f8a6e24625907fa7929b4ea2cf35e
first_name <ss329222@gmail.com>
2022-02-04 07:53:01 +0530

    added:subtraction

---
+++ understandable.java/src/javapractice/subtraction.java
@@ -0,0 +1,13 @@
+package javapractice;
+
+public class subtraction {
+
+	public static void main(String[] args) {
+		int a=4,b=6,c;
+		c=a-b;
+		System.out.println("subtraction of two numbers is:" +c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
